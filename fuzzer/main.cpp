905056ee7a26ebf5d26f3b4c7857e481bf237fba
Duy Tai Nguyen <duytai.cse@gmail.com>
2018-11-24 15:48:07 +0800

    add more options

--- fuzzer/main.cpp
+++ fuzzer/main.cpp
@@ -19,75 +19,109 @@ using namespace boost::filesystem;
 namespace pt = boost::property_tree;
 namespace po = boost::program_options;
 
+static int DEFAULT_MODE = 1; // AFL
+static int DEFAULT_DURATION = 600; // 10 mins
+static string DEFAULT_CONTRACTS_FOLDER = "contracts/";
+static string DEFAULT_ASSETS_FOLDER = "assets/";
+
+string compileSolFiles(string folder) {
+  stringstream ret;
+  path folderPath(folder);
+  for (auto& file : boost::make_iterator_range(directory_iterator(folderPath), {})) {
+    if (!is_directory(file.status()) && boost::ends_with(file.path().string(), ".sol")) {
+      string filePath = file.path().string();
+      ret << "solc";
+      ret << " --combined-json abi,bin,bin-runtime " + filePath;
+      ret << " > " + filePath + ".json";
+      ret << endl;
+    }
+  }
+  return ret.str();
+}
+
+
+string fuzzJsonFiles(string folder, int duration, int mode) {
+  stringstream ret;
+  path folderPath(folder);
+  for (auto& file : boost::make_iterator_range(directory_iterator(folderPath), {})) {
+    if (!is_directory(file.status()) && boost::ends_with(file.path().string(), ".json")) {
+      string filePath = file.path().string();
+      string fileName = file.path().filename().string();
+      string fileNameWithoutExtension = fileName.substr(0, fileName.length() - 9);
+      string contractName = fileNameWithoutExtension.find("_0x") != string::npos
+        ? fileNameWithoutExtension.substr(0, fileNameWithoutExtension.find("_0x"))
+        : fileNameWithoutExtension;
+      ret << "./fuzzer";
+      ret << " -f " + filePath;
+      ret << " -n " + contractName;
+      ret << " -d " + to_string(duration);
+      ret << " -m " + to_string(mode);
+      ret << endl;
+    }
+  }
+  return ret.str();
+}
+
 int main(int argc, char* argv[]) {
   /* Run EVM silently */
   dev::LoggingOptions logOptions;
   logOptions.verbosity = VerbositySilent;
   dev::setupLogging(logOptions);
   /* Program options */
+  int mode = DEFAULT_MODE;
+  int duration = DEFAULT_DURATION;
+  string contractsFolder = DEFAULT_CONTRACTS_FOLDER;
+  string assetsFolder = DEFAULT_ASSETS_FOLDER;
   string jsonFile = "";
   string contractName = "";
-  string contractsFolder = "contracts/";
-  int mode = 1;
-  int duration = 600;
   po::options_description desc("Allowed options");
+  po::variables_map vm;
+  
   desc.add_options()
     ("help,h", "produce help message")
-    ("scan,s", po::value(&contractsFolder), "scan and generate working script")
+    ("contracts,c", po::value(&contractsFolder), "contract's folder path")
+    ("generate,g", "generate fuzzMe script")
+    ("assets,a", po::value(&assetsFolder), "asset's folder path")
     ("file,f", po::value(&jsonFile), "fuzz a contract")
     ("name,n", po::value(&contractName), "contract name")
     ("mode,m", po::value(&mode), "choose mode: 0 - Random | 1 - AFL ")
     ("duration,d", po::value(&duration), "fuzz duration");
-  po::variables_map vm;
   po::store(po::parse_command_line(argc, argv, desc), vm);
   po::notify(vm);
-  
+  /* Show help message */
   if (vm.count("help")) {
-    cout << desc << "\n";
-    printf("Example: \n");
-    printf("> Scan contracts/ folder to create executable file\n");
-    printf("  " cGRN "./fuzzer -s contracts/" cRST "\n");
-    printf("> Fuzz one contract\n");
-    printf("  " cGRN "./fuzzer -s" cRST "\n");
+    stringstream output;
+    output << desc << endl;
+    output << "Example:" << endl;
+    output << "> Generate executable scripts" << endl;
+    output << "  " cGRN "./fuzzer -g" cRST << endl;
+    cout << output.str();
     return 0;
   }
-  if (vm.count("scan")) {
+  /* Generate working scripts */
+  if (vm.count("generate")) {
     std::ofstream fuzzMe("fuzzMe");
-    path p(contractsFolder);
-    int numContracts = 0;
-    /* List all .sol files */
-    stringstream os;
+    stringstream output;
     fuzzMe << "#!/bin/bash" << endl;
-    for (auto& file : boost::make_iterator_range(directory_iterator(p), {})) {
-      if (!is_directory(file.status()) && boost::ends_with(file.path().string(), ".sol")) {
-        string filePath = file.path().string();
-        string fileName = file.path().filename().replace_extension("").string();
-        fuzzMe << "solc --combined-json abi,bin,bin-runtime ";
-        fuzzMe << filePath;
-        fuzzMe << " > " << filePath + ".json" << endl;
-        string contractName = fileName.find("_0x") != string::npos ? fileName.substr(0, fileName.find("_0x")) : fileName;
-        os << "./fuzzer -f " + filePath + ".json";
-        os << " -n " + contractName;
-        os << " -d " + to_string(duration);
-        os << " -m " + to_string(mode);
-        os << "\n";
-        numContracts ++;
-      }
-    }
-    fuzzMe << os.str() << endl;
+    fuzzMe << compileSolFiles(contractsFolder);
+    fuzzMe << compileSolFiles(assetsFolder);
+    fuzzMe << fuzzJsonFiles(contractsFolder, duration, mode);
+    /* Show response */
+    output << cGRN "> Created \"fuzzMe\"" cRST "\n";
+    output << cGRN "> To run fuzz contracts:" cRST "\n";
+    output << "  chmod +x fuzzMe\n";
+    output << "  ./fuzzMe\n";
+    cout << output.str();
     fuzzMe.close();
-    printf(cGRN "> Created \"fuzzMe\" file with the following properties:" cRST "\n");
-    printf("  - Duration = %d\n", duration);
-    printf("  - Contracts = %d\n", numContracts);
-    printf(cGRN "> To run fuzz contracts:" cRST "\n");
-    printf("  chmod +x fuzzMe\n");
-    printf("  ./fuzzMe\n");
     return 0;
   }
+  /* Fuzz a single contract */
   if (vm.count("file") && vm.count("name") && vm.count("duration")) {
     std::ifstream file(jsonFile);
     if (!file.is_open()) {
-      cout << "[x] File " << jsonFile << " is not found " << endl;
+      stringstream output;
+      output << "[x] File " + jsonFile + " is not found" << endl;
+      cout << output.str();
       return 0;
     }
     /* Use ptree to read */
@@ -112,7 +146,7 @@ int main(int argc, char* argv[]) {
     fuzzParam.abiJson = root.get<string>(abiPath);
     fuzzParam.bin = root.get<string>(binPath);
     fuzzParam.binRuntime = root.get<string>(binRuntimePath);
-    fuzzParam.mode = mode == 1 ? AFL : RANDOM;
+    fuzzParam.mode = !mode ? RANDOM : AFL;
     fuzzParam.contractName = fullContractName;
     fuzzParam.duration = duration;
     Fuzzer fuzzer(fuzzParam);
