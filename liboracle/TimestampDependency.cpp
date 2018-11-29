da656bdcf35d52189326790103ac740d02ee02f1
Duy Tai Nguyen <duytai.cse@gmail.com>
2018-11-29 11:55:51 +0800

    timstamp depedency

--- liboracle/TimestampDependency.cpp
+++ liboracle/TimestampDependency.cpp
@@ -5,8 +5,31 @@ using namespace eth;
 using namespace std;
 
 namespace fuzzer {
-  bool timestampDependency(CallLog) {
-    return false;
+  bool timestampDependency(CallLog callLog) {
+    u256 numTimestamp = 0;
+    u256 numCallWithWei = 0;
+    u256 numTimestampDependency = 0;
+    for (auto callLogItem : callLog) {
+      auto type = callLogItem.type;
+      auto level = callLogItem.level;
+      if (type == CALL_OPCODE && !level) {
+        if (numTimestamp && numCallWithWei) {
+          numTimestampDependency += 1;
+        }
+        numTimestamp = 0;
+        numCallWithWei = 0;
+      } else {
+        if (type == TIMESTAMP_OPCODE) numTimestamp ++;
+        if (type == CALL_OPCODE) {
+          auto payload = callLogItem.payload;
+          if (payload.wei) {
+            numCallWithWei ++;
+          }
+        }
+      }
+    }
+    numTimestampDependency += (numTimestamp && numCallWithWei) ? 1 : 0;
+    return !!numTimestampDependency;
   }
 }
 
