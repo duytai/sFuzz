1d9d08b6cfe5c6652afbc5e8da3457243225a7de
Duy Tai Nguyen <duytai.cse@gmail.com>
2018-12-19 17:04:09 +0800

    update gaslessSend

--- liboracle/GaslessSend.cpp
+++ liboracle/GaslessSend.cpp
@@ -6,13 +6,12 @@ using namespace std;
 
 namespace fuzzer {
   bool gaslessSend(CallLog callLog) {
-    u256 sendGas = 2300;
     u256 numSend = 0;
     for (auto callLogItem : callLog) {
       auto level = callLogItem.level;
       auto inst = callLogItem.payload.inst;
       auto gas = callLogItem.payload.gas;
-      if (level > 0 && inst == Instruction::CALL && gas == sendGas) {
+      if (level > 0 && inst == Instruction::CALL && gas == 2300) {
         numSend ++;
       }
     }
