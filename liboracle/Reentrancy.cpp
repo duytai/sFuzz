7dd70c7cb8d1942032fdcd15bfcb1e681a5d0fd5
Tai Duy Nguyen <duytai.cse@gmail.com>
2019-02-14 21:49:24 +0800

    Update Reentrancy

--- liboracle/Reentrancy.cpp
+++ liboracle/Reentrancy.cpp
@@ -6,28 +6,14 @@ using namespace std;
 
 namespace fuzzer {
   bool Reentrancy::analyze(CallLog callLog) {
-    auto ethTransfer = false;
     auto isReentrancy = false;
     for (auto callLogItem : callLog) {
       auto data = callLogItem.payload.data;
       auto level = callLogItem.level;
-      auto inst = callLogItem.payload.inst;
-      auto wei = callLogItem.payload.wei;
-      if ((inst == Instruction::CALL
-      || inst == Instruction::CALLCODE
-      || inst == Instruction::DELEGATECALL
-      || inst == Instruction::STATICCALL
-      ) && level > 1 && wei > 0) {
-        ethTransfer = true;
-      }
-
       if (level >= 4 && toHex(data) == "000000ff") {
-        isReentrancy = true;
+        testData = callLog[0].payload.testData;
+        return true;
       }
-    }
-    if (ethTransfer && isReentrancy) {
-      testData = callLog[0].payload.testData;
-      return true;
     }
     return false;
   }
