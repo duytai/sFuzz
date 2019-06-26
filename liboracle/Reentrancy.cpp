510d864f3b9bba615e205a55f498c301eb82046e
leilis <leilis@qq.com>
2019-06-26 19:18:21 +0800

    fix: record right reentrance pc

--- liboracle/Reentrancy.cpp
+++ liboracle/Reentrancy.cpp
@@ -6,16 +6,30 @@ using namespace std;
 
 namespace fuzzer {
   bool Reentrancy::analyze(CallLog callLog) {
-      for (auto callLogItem : callLog) {
+    uint pos = 0;
+    bool ret = false;
+    for(uint i = 0; i < callLog.size(); i++) {
+      auto callLogItem = callLog[i];
       auto data = callLogItem.payload.data;
       auto level = callLogItem.level;
       if (level >= 4 && toHex(data) == "000000ff") {
         testData = callLog[0].payload.testData;
-        issuePayloadPc = callLogItem.payload.pc;
-        return true;
+        pos = i;
+        ret = true;
+        break;
       }
     }
-    return false;
+    if(pos > 0) {
+      for (uint i = pos - 1; i > 0; --i)
+      {
+         auto callLogItem = callLog[i];
+         if (callLogItem.level == 1) {
+            issuePayloadPc = callLogItem.payload.pc;
+            break;
+         }
+      }
+    }
+    return ret;
   }
 }
 
